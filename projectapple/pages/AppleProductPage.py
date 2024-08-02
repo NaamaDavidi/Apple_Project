b8cafd8c8f9509826de0099e80b73c2cb2df169d
NaamaDavidi <naamadavidi88@gmail.com>
2024-08-02 09:49:51 +0300

    added a test

--- projectapple/pages/AppleProductPage.py
+++ projectapple/pages/AppleProductPage.py
@@ -8,18 +8,31 @@ class Product_Page():
 
     def __init__(self,driver):
         self.driver = driver
-        self.price_offer = 'all-access-pass__container'
+        self.find_price_macbook = "p[class= 'welcome__lockup-primary-copy has-dynamic-content']"
+        self.find_price_airpods = "p[class='typography-compare-price pricing']"
 
 
-
-    def find_price_offer(self):
+    def find_price_offer_macbook(self):
         time.sleep(3)
-        Price_Offer = self.driver.find_element(By.CSS_SELECTOR,"p[class= 'welcome__lockup-primary-copy has-dynamic-content']")
+        Price_Offer = self.driver.find_element(By.CSS_SELECTOR,self.find_price_macbook)
         price_Offer_text = Price_Offer.text
         print(f'price found the value is {price_Offer_text}')
         price_as_str = price_Offer_text
         index1=price_as_str.index("or")-1
         index2 = price_as_str.index("$")+1
+        price_temp = price_as_str[index2:index1]
+        time.sleep(3)
+        print(price_temp)
+        price = int(price_temp)
+        return price
+
+    def find_price_offer_airpods(self):
+        time.sleep(3)
+        Price_Offer = self.driver.find_elements(By.CSS_SELECTOR,self.find_price_airpods)
+        price_Offer_text = Price_Offer[1].text
+        print(f'price found the value is {price_Offer_text}')
+        price_as_str = price_Offer_text
+        index2 = price_as_str.index("$")+3
         price_temp = price_as_str[index2:index1]
         time.sleep(3)
         print(price_temp)
