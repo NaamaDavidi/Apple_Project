f5bf60afa742e2a0275af8142aac46b82318e332
NaamaDavidi <naamadavidi88@gmail.com>
2024-08-21 00:04:30 +0300

    fixed names

--- Project_Apple/Tests/AppleTest.py
+++ Project_Apple/Tests/AppleTest.py
@@ -1,12 +1,8 @@
-import time
 import unittest
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
-from Project_Apple.Pages import AppleHomePage
 from Project_Apple.Pages.AppleBaseSelenium import ProjectBaseSelenium
-from Project_Apple.Pages.AppleGlobals import URL_BASE, limit1, limit2, item1, item3, item2
+from Project_Apple.Pages.AppleGlobals import URL_BASE, LIMIT_1, LIMIT_2, ITEM_1, ITEM_3, ITEM_2
 from Project_Apple.Pages.AppleProductPage import ProductPage
 from Project_Apple.Pages.AppleHomePage import HomePage
 from Project_Apple.Pages.AppleExplorePage import ExplorePage
@@ -33,10 +29,10 @@ class AppleTest(unittest.TestCase):
     """
     def testSearchMacbook(self):
         self.homePageObject.createSearch("macbook")
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, item1)))
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, ITEM_1)))
         self.explorePage.selectItemMacbook()
         price = self.productPage.findPriceOfferMacbook()
-        assert price>limit1,"the price is lower than 1400 not as expected"
+        assert price>LIMIT_1,"the price is lower than 1400 not as expected"
     """
     This test is responsible for finding the airpods price, and making sure the price is lower then the limit set.
     This test is a negative test.
@@ -44,10 +40,10 @@ class AppleTest(unittest.TestCase):
 
     def testSearchAirpods(self):
         self.homePageObject.createSearch("airpods")
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, item2)))
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, ITEM_2)))
         self.explorePage.selectItemAirpods()
         price = self.productPage.findPriceOfferAirpods()
-        assert price < limit2, "the price is higher than 150 not as expected"
+        assert price < LIMIT_2, "the price is higher than 150 not as expected"
 
     """
     This test is responsible for finding the iphone price, and making sure the price is higher then the limit set
@@ -56,10 +52,10 @@ class AppleTest(unittest.TestCase):
     """
     def testSearchIphone(self):
         self.homePageObject.createSearch("iphone")
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, item3)))
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, ITEM_3)))
         self.explorePage.selectItemIphone()
         price = self.productPage.findPriceOfferIphone()
-        assert price > limit1, f"the price of {item3} is cheaper than {item1}"
+        assert price > LIMIT_1, f"the price of {ITEM_3} is cheaper than {ITEM_1}"
 
 
 
