0defafa519428a0f73287cad52e2ce89f8d58282
NaamaDavidi <naamadavidi88@gmail.com>
2024-08-20 21:47:37 +0300

    Delete AppleGlobals.py

--- AppleGlobals.py
+++
@@ -1,8 +0,0 @@
-URL_BASE = "https://www.apple.com/"
-SearchText = any
-LIMIT_1 = 1400
-LIMIT_2 = 150
-ITEM_1 = 'macbook'
-ITEM_2 = 'airpods'
-ITEM_3 ='iphone'
-ITEM_TYPE = 'li[class="chapternav-item chapternav-item-iphone-15-pro"]'
